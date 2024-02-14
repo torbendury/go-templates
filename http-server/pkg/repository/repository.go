eddbae5f7f3c534fce330736a64e955cc9d26faf
Torben Dury <94752317+torbendury@users.noreply.github.com>
2024-02-14 12:10:21 +0000

    wip(http-server): interface for data repository

--- http-server/pkg/repository/repository.go
+++ http-server/pkg/repository/repository.go
@@ -1,8 +1,19 @@
 package repository
 
-type Repository interface {
-	Get()
-	Create()
-	Update()
-	Delete()
+import (
+	"errors"
+
+	"github.com/torbendury/go-templates/http-server/pkg/model"
+)
+
+var (
+	ErrNotFound      = errors.New("not found")
+	ErrAlreadyExists = errors.New("already exists")
+)
+
+type UserRepository interface {
+	Get(*model.User) (*model.User, error)
+	Create(*model.User) (*model.User, error)
+	Update(*model.User) (*model.User, error)
+	Delete(*model.User) error
 }
--- http-server/pkg/repository/repository_test.go
+++ http-server/pkg/repository/repository_test.go
@@ -1,1 +1,23 @@
 package repository
+
+import "github.com/torbendury/go-templates/http-server/pkg/model"
+
+type UserRepositoryMock struct {
+	Users []model.User
+}
+
+func (urm *UserRepositoryMock) Get(*model.User) (*model.User, error) {
+	return nil, nil
+}
+
+func (urm *UserRepositoryMock) Create(*model.User) (*model.User, error) {
+	return nil, nil
+}
+
+func (urm *UserRepositoryMock) Update(*model.User) (*model.User, error) {
+	return nil, nil
+}
+
+func (urm *UserRepositoryMock) Delete(*model.User) error {
+	return nil
+}
